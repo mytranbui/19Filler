f9805467a4fd8e417a7a85150b09ac5c09f50120
My Tran Bui <mytranbui@Mys-MacBook-Pro.local>
2020-11-22 19:12:02 +0100

    main start

--- filler.h
+++ filler.h
@@ -1,13 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.c                                             :+:      :+:    :+:   */
+/*   filler.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbui <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/18 19:22:10 by mbui              #+#    #+#             */
-/*   Updated: 2020/11/18 19:22:35 by mbui             ###   ########.fr       */
+/*   Created: 2020/11/22 19:00:32 by mbui              #+#    #+#             */
+/*   Updated: 2020/11/22 19:05:42 by mbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FILLER_H
+# define FILLER_H
+# include "libft/libft.h"
 
+typedef struct	s_coord
+{
+	int	x;
+	int	y;
+}		t_coord;
+
+/*typedef struct	s_tetro
+{
+
+}		t_tetro;
+
+typedef struct	s_map;
+{
+}		t_map;
+*/
+
+#endif
