fc36ee5827ff7048afa314e1a6ed21d444226fa6
My Tran Bui <mytranbui@Mys-MacBook-Pro.local>
2020-11-26 16:21:46 +0100

    debug print

--- src/main.c
+++ src/main.c
@@ -12,24 +12,63 @@
 
 #include "../filler.h"
 
-void	get_player(t_filler *filler)
+int		get_player(t_filler *filler)
 {
+	 FILE	*ID;
+
+
+	ID = fopen("play.txt","a");
+	 fprintf(ID,"LOL");
 	char 	*line;
 
 	get_next_line(0, &line);
-	filler->me = (line[10] == '1') ? 'O' : 'X';
-	filler->opp = (line[10] == '1') ? 'X' : 'O';
-	ft_strdel(&line);
+	if (line && !ft_strncmp(line, "$$$ exec p", 10) &&
+	(line[10] == '1' || line[10] == '2'))
+	{
+	 	fprintf(ID,"LOL");
+		filler->me = (line[10] == '1') ? 'O' : 'X';
+		filler->opp = (line[10] == '1') ? 'X' : 'O';
+		ft_strdel(&line);
+		FILE *ID = fopen("LOL.txt","a");
+	fprintf(ID,"f.me=%c\n", filler->me);
+	fprintf(ID,"f.opp=%c\n", filler->opp);
+		return (1);
+	}
+	return (-1);
 }
 
-void	debug(void)
+// void	get_mapsize(t_filler *filler)
+// {
+
+// }
+
+void	debug(int i)
+{
+	char	*line;
+	// int		i;
+	FILE	*ID;
+	
+	// i = 0;
+	ID = fopen("debug.txt","a");
+	get_next_line(0, &line);
+	fprintf(ID,"line[%d]=%s\n",i, line);
+	// while (line[i])
+	// {
+	// 	fprintf(ID,"line[%d]=%c\n",i, line[i]);
+	//  	i++;
+	// }
+	ft_strdel(&line);
+    fclose(ID);
+}
+
+void	debug2(void)
 {
 	char	*line;
 	int		i;
 	FILE	*ID;
 	
 	i = 0;
-	ID = fopen("debug.txt","a");
+	ID = fopen("debugi.txt","a");
 	//get_next_line(0, &line);
 	 while (get_next_line(0, &line) > -1)
 	//while (line[i])
@@ -47,14 +86,26 @@ int	main(void)
 {
 	t_filler	f;
 	int			i;
-	int			j;
 	char		*line;
 	//int			ret;
-	int			count_lines;
 	
 	i = 0;
-	j = 0;
-	count_lines = 0;
+	line = NULL;
+	ft_bzero(&f, sizeof(t_filler));
+	if (get_player(&f))
+	{
+		FILE *ID = fopen("LOL2.txt","a");
+		fprintf(ID,"f.me=%c\n", f.me);
+		fprintf(ID,"f.opp=%c\n", f.opp);
+	}
+	// while (!line)
+	//  // while (get_next_line(0, &line) > -1)
+	// {
+	// 	debug(i);
+	// 	// if (!ft_strncmp(line, "(null)", ft_strlen(line) - 6))
+	// 	// 	return (-1);
+	// 	i++;
+	// }
 	//if (!(f = (t_filler*)ft_memalloc(sizeof(t_filler))))
 	//	return (-1);
 	//init_filler(f);
@@ -69,13 +120,19 @@ int	main(void)
 	// 	ft_printf("\n------\nRet: %d\nLines: %d\n", ret, count_lines);
 	//f.me = (line[10] == '1') ? 'O' : 'X';
 	//f.opp = (line[10] == '1') ? 'X' : 'O';
-	get_player(&f);
-	FILE *ID = fopen("debug.txt","a");
-	fprintf(ID,"f.me=%c\n", f.me);
-	fprintf(ID,"f.opp=%c\n", f.opp);
-	line = NULL;
-	//while (!line)
-		debug();
+	// FILE *ID = fopen("debug.txt","a");
+	// fprintf(ID,"f.me=%c\n", f.me);
+	// fprintf(ID,"f.opp=%c\n", f.opp);
+	// // if (!ft_strncmp(line, "Plateau ", 8))
+	// // line = NULL;
+	// while (!line)
+	//  // while (get_next_line(0, &line) > -1)
+	// {
+	// 	debug(i);
+	// 	// if (!ft_strncmp(line, "(null)", ft_strlen(line) - 6))
+	// 	// 	return (-1);
+	// 	i++;
+	// }
 	// get_next_line(0, &line);
 	// while (line[j])
 	// {
